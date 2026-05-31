9679dc69ab8605ef2509bae14228a7335fffd8c9
pyatr <44798005+pyatr@users.noreply.github.com>
2026-05-31 13:46:34 +0700

    Pug packages

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-<template>
+<template lang="pug">
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
